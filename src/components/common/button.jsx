a2a84e0b337704ded871e1cfe793c953f54cb949
NelakaWith <nelaka.with@gmail.com>
2025-10-09 18:07:54 +0530

    feat(button): enhance button component with icon support and customizable styles; remove unused logo component feat(header): integrate button with icon in header component; import new icon component feat(icon): add arrowCircleUpright icon component and SVG file

--- src/components/common/button.jsx
+++ src/components/common/button.jsx
@@ -1,7 +1,65 @@
-function Button() {
+function Button({
+  text = "Button",
+  type = "primary",
+  icon = null,
+  size = "md",
+  className = "",
+  ...props
+}) {
+  const baseClasses = "inline-flex items-center gap-2 rounded-full";
+
+  const renderIcon = () => {
+    if (!icon) return null;
+    if (typeof icon === "string") {
+      return (
+        <img
+          src={icon}
+          alt=""
+          aria-hidden="true"
+          className="w-6 h-6 inline-block"
+        />
+      );
+    }
+    return (
+      <span
+        className={`w-8 h-8 flex items-center justify-center ${iconColorClass}`}
+      >
+        {icon}
+      </span>
+    );
+  };
+
+  const sizeClasses = {
+    sm: "px-3 py-2",
+    md: "px-4 py-2",
+    lg: "px-6 py-4",
+  };
+
+  const typeClasses = {
+    primary:
+      "bg-brand-300 text-white hover:bg-brand-200 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-brand-500",
+    secondary:
+      "bg-white text-brand-300 hover:bg-neutral-200 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-brand-300",
+    outline:
+      "bg-transparent border border-neutral-500 text-neutral-500 hover:bg-neutral-100 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-neutral-500",
+  };
+
+  const iconColor = {
+    primary: "text-white",
+    secondary: "text-brand-300",
+    outline: "text-neutral-500",
+  };
+
+  const iconColorClass = iconColor[type] || "text-white";
+
   return (
-    <button>
-      <span></span>
+    <button
+      type={type}
+      className={`${baseClasses} ${className} ${sizeClasses[size]} ${typeClasses[type]}`}
+      {...props}
+    >
+      <span>{text}</span>
+      {renderIcon()}
     </button>
   );
 }
--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Button from "../common/button";
+import IconArrowCircleUpright from "../vectors/arrowCircleUpright";
 
 function Header() {
   return (
     <header className="rounded-full px-6 py-5 glass-xl fixed start-0 end-0 top-5 mx-5 my-2 flex justify-between items-center">
       <Image src="/logo.svg" alt="Logo" width={40} height={40} />
+      <Button text="Book Now" icon={<IconArrowCircleUpright size={24} />} />
       <Image src="/hamburger.svg" alt="Menu" width={40} height={40} />
     </header>
   );
