46b12ed1c17604ec6b2839458a3a5f5207979fcd
NelakaWith <nelaka.with@gmail.com>
2025-10-11 14:21:37 +0530

    feat(partners): Enhance Partners section with scroll fade effect and scrollbar hiding

--- src/components/sections/partners.jsx
+++ src/components/sections/partners.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+
 const partnerLogos = [
   "/partnerLogo01.svg",
   "/partnerLogo02.svg",
@@ -13,17 +14,19 @@ function Partners() {
       <h2 className="text-neutral-400 text-2xl text-center">
         Trusted by global brands
       </h2>
-      <div className="inline-flex justify-center items-center overflow-x-auto w-full mt-8">
-        {partnerLogos.map((logo, index) => (
-          <Image
-            key={index}
-            src={logo}
-            alt={`Partner Logo ${index + 1}`}
-            className="m-4 max-w-[150px] h-auto"
-            width={150}
-            height={150}
-          />
-        ))}
+      <div className="relative scroll-fade mt-8">
+        <div className="px-4 inline-flex items-center overflow-x-auto w-full no-scrollbar">
+          {partnerLogos.map((logo, index) => (
+            <Image
+              key={index}
+              src={logo}
+              alt={`Partner Logo ${index + 1}`}
+              className="m-4 max-w-[150px] h-auto"
+              width={150}
+              height={150}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
