42db00754786b831526610860760338f8deb6b4c
NelakaWith <nelaka.with@gmail.com>
2025-10-22 09:57:01 +0530

    fix(header): add z-index to header for proper stacking context

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -4,7 +4,7 @@ import IconArrowCircleUpright from "../vectors/arrowCircleUpright";
 
 function Header() {
   return (
-    <header className="rounded-full px-6 py-5 glass-xl fixed start-0 end-0 top-5 mx-5 my-2 flex justify-between items-center">
+    <header className="rounded-full px-6 py-5 glass-xl fixed start-0 end-0 top-5 z-10 mx-5 my-2 flex justify-between items-center">
       <Image src="/logo.svg" alt="Logo" width={40} height={40} />
       <Button text="Book Now" icon={<IconArrowCircleUpright size={24} />} />
       <Image src="/hamburger.svg" alt="Menu" width={40} height={40} />
