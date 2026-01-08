0f74a7f543c7e77f192755aa5984da45639d1832
NelakaWith <nelaka.with@gmail.com>
2026-01-08 22:25:27 +0530

    feat(appFooter): enhance layout and responsiveness for footer section with improved alignment and spacing

--- src/components/common/appFooter.jsx
+++ src/components/common/appFooter.jsx
@@ -3,28 +3,29 @@ import IconifyClient from "./IconifyClient";
 
 function Footer() {
   return (
-    <footer className="py-16 relative overflow-hidden">
-      <div className="px-8">
-        <div className="flex">
+    <footer className="py-16 md:py-8 md:mt-52 relative overflow-hidden">
+      <div className="px-8 md:flex md:justify-between">
+        <div className="flex md:items-center">
           <Image src="/logoWhiteFull.svg" alt="Logo" width={164} height={48} />
-          <div className="border-e border-neutral-400 mx-6"></div>
-          <p className="text-xs text-neutral-400">
+          <div className="border-e border-neutral-400 mx-6 h-12"></div>
+          <p className="text-xs text-neutral-400 md:max-w-60">
             Solutions that drive success and propel your business forward
           </p>
         </div>
-        <div className="flex gap-2 mt-8 justify-between text-lg font-medium">
+        <div className="flex gap-2 md:gap-8 mt-8 justify-between text-lg font-medium">
           <span>Features</span>
           <span>Integration</span>
           <span>Pricing</span>
         </div>
       </div>
-      <div className="mx-8 mt-8 flex flex-col-reverse text-center">
-        <p className="text-xs mt-8">
+      <div className="hidden md:block mx-8 my-9 border-b border-neutral-400"></div>
+      <div className="mx-8 mt-8 flex flex-col-reverse md:flex-row md:items-center md:justify-between text-center">
+        <p className="text-xs mt-8 md:text-start">
           Copyright © 2024 Designed by Flotr. <br />
           Developed by @NelakaWith. <br />
           All Rights Reserved
         </p>
-        <div className="flex gap-2 mt-8 justify-between text-lg font-medium">
+        <div className="flex gap-2 md:gap-8 mt-8 justify-between text-lg font-medium">
           <span>Privacy Policy</span>
           <span>Terms of Use</span>
         </div>
@@ -51,7 +52,6 @@ function Footer() {
           />
         </div>
       </div>
-      <div className="hero-glow h-[600px] absolute -bottom-80 -left-20 -right-20 -z-10"></div>
     </footer>
   );
 }
